c553fb050a461b2a45237e31b3202269f05caed6
Luis Francisco <lgarciamalagan@gmail.com>
2023-11-30 23:38:34 -0600

    first commit

---
+++ constants/index.js
@@ -0,0 +1,114 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/captura 1.png",
+    title: "Lobby",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/captura 2.png",
+    title: "Sala E",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/captura 3.png",
+    title: "Sala B",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/captura 4.png",
+    title: "Planta Alta",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/captura 5.png",
+    title: "Sala I",
+  },
+];
+
+export const startingFeatures = [
+  "Pablo Odín López Holguín",
+  "Luis Francisco García Malagón",
+  "Kevin Yael Ruiz Sandoval",
+  "Fidel Alejandro Izquierdo Velasco",
+];
+
+/*export const developers = [
+  {
+    imgUrl: '/owner.svg',
+    name: 'Pablo Odín López Holguín',
+    job: 'Project Manger',
+  },
+  {
+    imgUrl: '/web-developer.svg',
+    name: 'Luis Francisco García Malagón',
+    job: 'Web designer/developer',
+  }
+]*/
+
+export const newFeatures = [
+  {
+    imgUrl: "/headset.svg",
+    title: "Simulacion de diferentes escenarios",
+    subtitle:
+      "Tales como terremotos, inundaciones, incendios forestales, tormentas, huracanes, tsunamis, entre otros. ",
+  },
+  {
+    imgUrl: "/vrpano.svg",
+    title: "Personalización de escenarios",
+    subtitle:
+      "Escenarios completamente al gusto del cliente asi como la información para adaptarse a las características específicas de cada comunidad y organización.",
+  },
+  {
+    imgUrl: "/info.svg",
+    title: "Información detallada",
+    subtitle:
+      "Acerca de cómo prepararse antes, durante y después de cada tipo de desastre, incluyendo los procedimientos de evacuación y los primeros auxilios.",
+  },
+  {
+    imgUrl: "/interact.svg",
+    title: "Completa interaccion",
+    subtitle:
+      "Permitiendo que puedan practicar los procedimientos de seguridad en un ambiente inmersivo y realista.",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/source-logo-resize.png",
+    title: "Source",
+    subtitle:
+      "Source es un motor de juegos 3D creado por Valve en 2004 y es el sucesor de GoldSrc. La fuente ha sido utilizada por Valve, empresas de terceros y (al igual que GoldSrc) innumerables modificaciones comunitarias por parte de personas de todo el mundo.",
+  },
+  {
+    imgUrl: "/Java-logo2.png",
+    title: "Java",
+    subtitle:
+      "Java es un lenguaje de programación orientado a objetos y una plataforma de software ampliamente utilizado que se ejecuta en miles de millones de dispositivos, que incluyen computadoras portátiles, dispositivos móviles, consolas de juegos, dispositivos médicos y muchos otros.",
+  },
+  {
+    imgUrl: "/blender-logo.png",
+    title: "Blender",
+    subtitle:
+      "Blender es un conjunto de herramientas de creación de contenido 3D totalmente integrada, que ofrece una amplia gama de herramientas esenciales, que incluyen Modelado, Procesamiento, Animación y sistemas de control, Edición de video, Efectos visuales, Composición, Texturizado y varios tipos de Simulaciones dinámicas.",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
